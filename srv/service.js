d7910a6c4cb07aa9f272d67c8b4f86d70137f37e
sergio barros <sbb_20@yahoo.com.br>
2026-05-10 16:33:12 +0000

    Teste

--- srv/service.js
+++ srv/service.js
@@ -1,6 +1,7 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async (srv) => {
+
     // Connect to the external service definition
     const remoteService = await cds.connect.to('API_BUSINESS_PARTNER'); 
 
@@ -9,4 +10,5 @@ module.exports = cds.service.impl(async (srv) => {
         // Forward the query to the remote service
         return remoteService.run(req.query);
     });
+    
 });
